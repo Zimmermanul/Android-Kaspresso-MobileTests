1534657272a99659ae743df7eb5b77f10aec2e0a
Max Khabibullin <habibullinmax@gmail.com>
2023-07-29 15:55:33 +0500

    fixincrementButtonShouldIncreaseCounterValueTest

--- app/src/androidTest/java/com/example/kaspresso/IncrementButtonTest.kt
+++ app/src/androidTest/java/com/example/kaspresso/IncrementButtonTest.kt
@@ -1,6 +1,7 @@
 package com.example.kaspresso
 
 import androidx.test.ext.junit.rules.activityScenarioRule
+import com.example.kaspresso.MainScreen.counterText
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
@@ -13,11 +14,16 @@ class IncrementButtonTest : TestCase(kaspressoBuilder = Kaspresso.Builder.simple
 
     @Test
     fun incrementButtonShouldIncreaseCounterValue() = run {
-    step("Increase counter value to 2"){
-        MainScreen {
-
+        step("Increase counter value to 2") {
+            MainScreen {
+                repeat(2) {
+                    incrementButton.click()
+                }
+            }
         }
-    }
+        step("Assert that counter value equals 2") {
+            counterText.hasText("Count: 2")
+        }
     }
 
 }
